72ca238649167f6ff4f89431cd96003a09688bdb
송강찬 <76604142+KangChanSong@users.noreply.github.com>
2022-04-17 17:48:34 +0900

    Prometheus & Grafana 사용해보기

---
+++ CentralizedMetricsExample/src/main/java/egovframework/msa/sample/CentralizedMetricsExampleApplication.java
@@ -0,0 +1,12 @@
+package egovframework.msa.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralizedMetricsExampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CentralizedMetricsExampleApplication.class, args);
+    }
+
+}
